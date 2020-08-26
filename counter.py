2756b4f812fa0ece36d71dda0ab72d37008e790d
Yohei-Mizuno <mizyotama2525@gmail.com>
2020-08-26 19:17:36 +0900

    #カウントアプリ 1, 関数内に value を加える → 変数 = Counter() の () の中に任意の数字を入れると value が 任意の数字になる 増える仕組み 2, increase関数を作成 increase関数 を実行すると value が 1 増える

---
+++ counter.py
@@ -0,0 +1,18 @@
+class Counter:
+    def __init__(self, value):
+        self.value = value
+
+    def increase(self):
+        self.value += 1
+
+
+if __name__ == '__main__':
+
+    counter_1 = Counter(0)
+    print(counter_1.value)
+
+    counter_1.increase()
+    print(counter_1.value)
+
+    counter_2 = Counter(1)
+    print(counter_2.value)
